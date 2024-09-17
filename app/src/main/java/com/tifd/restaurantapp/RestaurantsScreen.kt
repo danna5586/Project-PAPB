0756015ff2fb58d1ee04acf425ca39defcfb8ae9
Ashbahul Danna <ashbahul.danna@gmail.com>
2024-09-17 22:41:54 +0700

    HAI

---
+++ app/src/main/java/com/tifd/restaurantapp/RestaurantsScreen.kt
@@ -0,0 +1,110 @@
+package com.tifd.restaurantapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.alpha
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RestaurantsScreen() {
+    val viewModel: RestaurantsViewModel = viewModel()
+    Column(
+        modifier = Modifier.padding(2.dp)
+    ) {
+        Text(
+            text = "Nama : Ashbahul Danna Yunas",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            text = "NIM : 225150201111022",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        LazyColumn(
+            contentPadding = PaddingValues(
+                vertical = 8.dp,
+                horizontal = 8.dp
+            )
+        ) {
+            items(viewModel.state.value) { restaurant ->
+                RestaurantItem(restaurant) { id ->
+                    viewModel.toggleFavorite(id)
+                    }
+            }
+        }
+    }
+}
+
+@Composable
+fun RestaurantItem(item: Restaurant, onClick: (id: Int) -> Unit) {
+    val icon = if (item.isFavorite)
+        Icons.Filled.Favorite
+    else
+        Icons.Filled.FavoriteBorder
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            RestaurantIcon(Icons.Filled.Place, Modifier.weight(0.15f))
+            RestaurantDetails(item.title, item.description, Modifier.weight(0.7f))
+            RestaurantIcon(icon, Modifier.weight(0.15f)) {
+                onClick(item.id)
+            }
+        }
+    }
+}
+
+@Composable
+private fun RestaurantIcon(icon: ImageVector, modifier: Modifier, onClick: () -> Unit = { }) {
+    Image(
+        imageVector = icon,
+        contentDescription = "Restaurant icon",
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { onClick() })
+}
+
+@Composable
+private fun RestaurantDetails(title: String, description: String, modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.alpha(0.6f)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        RestaurantsScreen()
+    }
+}
