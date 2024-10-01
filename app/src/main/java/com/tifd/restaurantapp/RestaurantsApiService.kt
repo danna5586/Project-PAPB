07f3ad0bba1de577abc377dd646077a7f0835cf9
Ashbahul Danna <ashbahul.danna@gmail.com>
2024-10-01 23:12:06 +0700

    HAI lagi

---
+++ app/src/main/java/com/tifd/restaurantapp/RestaurantsApiService.kt
@@ -0,0 +1,10 @@
+package com.tifd.restaurantapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface RestaurantsApiService {
+    @GET("restaurants.json")
+    fun getRestaurants(): Call<List<Restaurant>>
+}
--- app/src/main/java/com/tifd/restaurantapp/RestaurantsViewModel.kt
+++ app/src/main/java/com/tifd/restaurantapp/RestaurantsViewModel.kt
@@ -3,10 +3,29 @@ package com.tifd.restaurantapp
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
+class RestaurantsViewModel(
+    private val stateHandle: SavedStateHandle
+) : ViewModel() {
+    private var restInterface: RestaurantsApiService
+    val state = mutableStateOf(emptyList<Restaurant>())
+    private lateinit var restaurantsCall: Call<List<Restaurant>>
 
-class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
-    val state = mutableStateOf(dummyRestaurants.restoreSelections())
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://papb-restaurantsapp-70b18-default-rtdb.firebaseio.com/")
+            .build()
+        restInterface = retrofit.create(
+            RestaurantsApiService::class.java
+        )
+        getRestaurants()
+    }
 
     fun toggleFavorite(id: Int) {
         val restaurants = state.value.toMutableList()
@@ -18,11 +37,13 @@ class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewMode
     }
 
     private fun storeSelection(item: Restaurant) {
-        val savedToggled = stateHandle.get<List<Int>?>(FAVORITES)
-            .orEmpty().toMutableList()
-        if (item.isFavorite) savedToggled.add(item.id)
-        else savedToggled.remove(item.id)
+        val savedToggled = stateHandle.get<List<Int>?>(FAVORITES).orEmpty().toMutableList()
+        if (item.isFavorite) savedToggled.add(item.id) else savedToggled.remove(item.id)
         stateHandle[FAVORITES] = savedToggled
+    }
+
+    companion object {
+        const val FAVORITES = "favorites"
     }
 
     private fun List<Restaurant>.restoreSelections(): List<Restaurant> {
@@ -36,8 +57,23 @@ class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewMode
         return this
     }
 
-    companion object {
-        const val FAVORITES = "favorites"
+    private fun getRestaurants() {
+        restaurantsCall = restInterface.getRestaurants()
+        restaurantsCall.enqueue(object : Callback<List<Restaurant>> {
+            override fun onResponse(call: Call<List<Restaurant>>, response: Response<List<Restaurant>>) {
+                response.body()?.let { restaurants ->
+                    state.value = restaurants.restoreSelections()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        restaurantsCall.cancel()
+    }
 }
